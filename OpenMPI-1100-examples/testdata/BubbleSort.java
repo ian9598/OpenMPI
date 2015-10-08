8495fa76d9e674aca1430f5bddba39bf7aeb96de
sengcheav <sengcheav@users.noreply.github.com>
2015-10-08 23:25:40 +1300

    Update BubbleSort.java

--- OpenMPI-1100-examples/testdata/BubbleSort.java
+++ OpenMPI-1100-examples/testdata/BubbleSort.java
@@ -194,7 +194,7 @@ class BubbleSort {
 		System.out.println ("++++++++");
 		BubbleSort.BubbleSort(array2 , c ); 
 		MPI.COMM_WORLD.send(array2, array2.length,  MPI.INT, 0, tag);
-	}*/else if (myrank ==3){System.out.println("EEEEEEEEEEEEEEE"); 
+	}*/else if (myrank ==1){System.out.println("EEEEEEEEEEEEEEE"); 
 		int boundSize  = (filesize/size ) ; 
 		int lowerBound = boundSize * myrank  ; // ( 0.25 * 0)
 		int upperBound = (boundSize * (myrank+1))   ;  // ( 0.25*1)	
@@ -240,7 +240,7 @@ class BubbleSort {
 	   // MPI.COMM_WORLD.recv(array1, array1.length,  MPI.INT, 1, tag);
 	   // MPI.COMM_WORLD.recv(array2, array2.length,  MPI.INT, 2, tag);
 	    //	MPI.COMM_WORLD.recv(empty, empty.length,  MPI.INT, 3, tag);
-	    MPI.COMM_WORLD.recv(array3, array3.length,  MPI.INT, 3, tag);
+	    MPI.COMM_WORLD.recv(array3, array3.length,  MPI.INT, 1, tag);
 	    for ( int i = 0 ; i< array3.length ; i++ ){
 	    	System.out.println(array3[i]+", " + i + " "+ array3.length );
 	    }	
