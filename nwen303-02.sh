360ec22d4a1cfd6918bfafc7aaeb933dbde11c29
Seng Leat <leatseng@greta-pt.ecs.vuw.ac.nz>
2015-10-11 16:20:03 +1300

    round

--- nwen303-02.sh
+++ nwen303-02.sh
@@ -3,16 +3,14 @@
 #$ -S /bin/sh
 #$ -N nwen303_test
 #$ -wd /vol/grid-solar/sgeusers/leatseng
-#$ -pe nwen303_1.pe 4
+#$ -pe nwen303_1.pe 6
 #
 echo ==UNAME==
 uname -n
 /usr/pkg/bin/mpirun -np $NSLOTS \
  /usr/pkg/java/sun-7/bin/java   \
-   -classpath /u/students/leatseng/NWEN303_2015T2/OpenMPI-1100-examples/testdata \SelectionSort 0
- 
-
-
+   -classpath /u/students/leatseng/NWEN303_2015T2/OpenMPI-1100-examples/testdata \
+SelectionSortONE 6
 
 
 
